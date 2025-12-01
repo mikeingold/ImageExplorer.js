bad313087903dc8a150fa3066e74a0e97ae84c85
Michael Ingold <mike.ingold@gmail.com>
2025-11-30 19:39:09 -0500

    Create modular lower map package

---
+++ maps/lower/map.js
@@ -0,0 +1,19 @@
+const MAP = {
+    "name": "Lower",
+    "description": "Lower side of PCB.",
+    "image": "maps/lower/lower.png",
+    "annotations": [
+        {
+            "id": "2001",
+            "name": "CPU",
+            "description": "Model ATMEGA328P-AU",
+            "coordinates": [[435, 820], [625, 820], [623, 424], [435, 424]]
+        },
+        {
+            "id": "2002",
+            "name": "Reset Switch",
+            "description": "Button for resetting system.",
+            "coordinates": [[259, 307], [412, 307], [417, 184], [261, 183]]
+        }
+    ]
+‎}
