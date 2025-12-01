e5ef85d9f8523accccd6512e994a044f03e2d5eb
Michael Ingold <mike.ingold@gmail.com>
2025-11-30 19:34:45 -0500

    Create modular upper map package

---
+++ maps/upper/map.js
@@ -0,0 +1,31 @@
+const MAP = {
+    "name": "Upper",
+    "description": "Upper side of PCB.",
+    "image": "maps/upper/upper.png",
+    "annotations": [
+        {
+            "id": "1000",
+            "name": "Power Connector",
+            "description": "Connects to power cable.",
+            "coordinates": [[84, 23], [85, 256], [132, 256], [130, 238], [220, 237], [221, 24]]
+        },
+        {
+            "id": "1001",
+            "name": "USB Connector",
+            "description": "Connects to USB cable.",
+            "coordinates": [[538, 54], [533, 162], [693, 161], [690, 52]]
+        },
+        {
+            "id": "1002",
+            "name": "Reset Button",
+            "description": "Button to reset device.",
+            "coordinates": [[704, 229], [838, 231], [840, 105], [701, 112]]
+        },
+        {
+            "id": "1003",
+            "name": "CPU",
+            "description": "Main processor.",
+            "coordinates": [[354, 886], [461, 997], [566, 879], [455, 780]]
+        },
+    ]
+‎}
